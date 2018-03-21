6541a25a670089efd551f4cbbe5fdda0aab984d9
Fabian <fabian_57@yahoo.it>
2018-03-21 23:50:37 +0100

    Basic main class created

--- Tester.py
+++ Tester.py
@@ -1,44 +1,22 @@
 from PerceptronVoted import PerceptronVoted
+from DatasetsFactory import DatasetsFactory
 import numpy as np
 
-
-def createDataset1():
-    x = np.array([
-        [1, 90],
-        [2, 45],
-        [3, 88],
-        [2, 16],
-        [1, 8],
-        [1, 9],
-        [1, 12],
-        [1, 19],
-        [2, 24],
-        [2, 19],
-        [3, 50],
-        [2, 15],
-        [18, 2],
-        [40, 3],
-        [19, 2],
-        [30, 3],
-        [26, 2],
-        [29, 3],
-        [38, 4],
-        [59, 1],
-        [12, 1],
-        [95, 4],
-        [32, 2],
-        [27, 1],
-    ], dtype=float)
-    y = np.array([-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ], dtype=float)
-    return x, y
+def test(x, y, ephocs, to_predict):
+    myVoted = PerceptronVoted()
+    myVoted.train(x, y, ephocs)
+    for data_to_predict in to_predict:
+        result = myVoted.predict(data_to_predict)
+        print("Data: ", data_to_predict, " Received: ", result)
 
 
-x, y = createDataset1()
-ephocs = 5
-myVoted = PerceptronVoted()
-myVoted.train(x, y, ephocs)
+print("\nsimple dataset")
+x, y = DatasetsFactory.simplePoints()
 to_predict = [[66, 1], [345, 2], [222, 3], [1, 57], [3, 300], [8, 560]]
-print("Numbers upper y=x: 1.0, lower y=x: -1.0")
-for data_to_predict in to_predict:
-    result = myVoted.predict(data_to_predict)
-    print("Data: ", data_to_predict, " Received: ", result)
+test(x, y, 5, to_predict)
+
+print("\nrealistic Dataset")
+path = "myPath"
+to_predict = [[-1]]
+x, y = DatasetsFactory.realisticDataset(path)
+test(x, y, 5, to_predict)

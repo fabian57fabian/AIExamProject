31fe03eb8492f1a5df0f89e2c263bd4d556fd98e
Fabian <fabian_57@yahoo.it>
2018-04-09 20:55:07 +0200

    Working perceptron done.

--- PerceptronSimple.py
+++ PerceptronSimple.py
@@ -1,38 +1,44 @@
 import numpy as np
 from PerceptronAbstract import PerceptronAbstract
+from numpy import dot
 
 
 class PerceptronSimple(PerceptronAbstract):
 
     def __init__(self, lr=1):
         super().__init__()
-        self.W = 0
+        self.w = np.array(0)
+        self.b = np.array(0)
         self.lr = lr
         self.trained = False
-
-    def activation_fn(self, x):
-        return 1 if x >= 0 else 0
 
     def predict(self, x):
         if not self.trained:
             return 0
-        z = self.W.T.dot(x)
-        a = self.activation_fn(z)
-        return a
+        z = dot(self.w, x) + self.b
+        return self.sign(z)
 
     def get_weights(self):
-        return self.W
+        return self.w
 
-    def train(self, X, y, epochs_t=0.1):
-        self.W = np.zeros(len(X[0]))
-        print(len(X[0]))
+    def train(self, X, Y, epochs_t=1):
+        R = len(X[0])
+        self.w = np.zeros(R)
+        self.b = k = 0
         for _ in range(epochs_t):
-            for i in range(len(y)):
-                x = X[i]
-                y_new = self.predict(x)
-                e = y[i] - y_new
-                b = self.lr * e * x
-                if b != []:
-                    self.W += b
-        print(self.W)
+            n_err = 0
+            for x, y in zip(X, Y):
+                if y * (dot(self.w, x) + self.b) <= 0:
+                    self.w += y * x
+                    self.b += y * R * R
+                    k += 1
+                    n_err += 1
         self.trained = True
+
+    def sign(self, x):
+        if x == 0:
+            return 0
+        if x > 0:
+            return 1
+        if x < 0:
+            return -1
